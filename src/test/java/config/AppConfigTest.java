6b6df66c7151bc464a6f7cf9531994033a5f6054
wongaz <wongaz@rose-hulman.edu>
2019-02-16 17:40:53 -0500

    guice started

--- src/test/java/config/AppConfigTest.java
+++ src/test/java/config/AppConfigTest.java
@@ -7,43 +7,73 @@ import org.junit.Test;
 import java.io.InputStream;
 
 public class AppConfigTest {
-    private static AppConfig appConfig1, appConfig2;
-
+    private static AppConfig moonConfig, appConfig2, nPipeConfig;
+    private static PipelineConfig moonPipeline, samplePipeline;
     @BeforeClass
     public static void setUp(){
         InputStream stream1 = Thread.currentThread()
                 .getContextClassLoader()
-                .getResourceAsStream("configFiles/test.yaml");
-        appConfig1 = AppLoader.loadConfiguration(stream1);
+                .getResourceAsStream("configFiles/moon.yaml");
+        moonConfig = AppLoader.loadConfiguration(stream1);
+        moonPipeline = moonConfig.getPipelineConfigs()[0];
 
         InputStream stream2= Thread.currentThread()
                 .getContextClassLoader()
                 .getResourceAsStream("configFiles/sample.yaml");
         appConfig2 = AppLoader.loadConfiguration(stream2);
+        samplePipeline = appConfig2.getPipelineConfigs()[0];
+
+        InputStream stream3= Thread.currentThread()
+                .getContextClassLoader()
+                .getResourceAsStream("configFiles/2Pipes.yaml");
+        nPipeConfig = AppLoader.loadConfiguration(stream3);
     }
 
     @Test
     public void testAppLoader(){
-        Assert.assertNotNull(appConfig1);
+        Assert.assertNotNull(moonConfig);
         Assert.assertNotNull(appConfig2);
+        Assert.assertNotNull(nPipeConfig);
     }
 
     @Test
     public void testExtractType() {
+        Assert.assertEquals("file", moonPipeline.getExtractType());
+        Assert.assertEquals("file",samplePipeline.getExtractType());
 
     }
 
     @Test
+    public void testPipelineName(){
+        Assert.assertEquals("moon", moonPipeline.getPipelineName());
+        Assert.assertEquals("simple",samplePipeline.getPipelineName());
+    }
+
+    @Test
+    public void testNumberOfPipelineConfig(){
+        Assert.assertEquals(1,moonConfig.getPipelineConfigs().length);
+        Assert.assertEquals(1,appConfig2.getPipelineConfigs().length);
+        Assert.assertEquals(2, nPipeConfig.getPipelineConfigs().length);
+    }
+
+    @Test
     public void testExtractConfiguration() {
+        String fileName = samplePipeline.getExtractConfiguration().get("file");
+        Assert.assertEquals("STLFiles/simple.stl", fileName);
 
     }
 
     @Test
     public void testParseFormat() {
+        Assert.assertEquals("STL", moonPipeline.getParseFormat());
+        Assert.assertEquals("STL", samplePipeline.getParseFormat());
+
     }
 
     @Test
     public void testLoadType() {
+        Assert.assertEquals("STL", moonPipeline.getParseFormat());
+        Assert.assertEquals("STL", samplePipeline.getParseFormat());
 
     }
 
